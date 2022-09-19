bf62d82cc7a540d01181e9dd76d77b0076c0441b
Niko H <acc.hellerniko@gmail.com>
2022-09-19 22:13:26 +0200

    updated packages added interface export bumped version to 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './decorators/task.decorators';
 export * from './decorators/worker.decorators';
 export * from './graphile-worker.module';
+export * from './interfaces/module-config.interfaces';
+export * from './interfaces/worker.interfaces';
 export * from './services/worker.service';
