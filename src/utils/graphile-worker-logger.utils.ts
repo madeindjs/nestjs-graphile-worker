606c4c6ae03671437d74a8cd55f996ec755ab926
Vahe Karufanyan <vahe.karufanyan@inconceptlabs.com>
2026-02-16 15:16:24 +0400

    fix: prevent undefined appearing after debug logs in Graphile Worker

--- src/utils/graphile-worker-logger.utils.ts
+++ src/utils/graphile-worker-logger.utils.ts
@@ -2,9 +2,16 @@ import type { LogFunction, LogLevel, LogMeta } from "@graphile/logger";
 import { Logger as GraphileLogger } from "@graphile/logger";
 import { Logger } from "@nestjs/common";
 
-function graphileWorkerLogFactory(): LogFunction {
-  const logger = new Logger("GraphileWorker");
+export interface NestLikeLogger {
+  debug: (message: string) => void;
+  error: (message: string) => void;
+  log: (message: string) => void;
+  warn: (message: string) => void;
+}
 
+export function createGraphileWorkerLogFunction(
+  logger: NestLikeLogger,
+): LogFunction {
   return (level: LogLevel, message: string, meta?: LogMeta) => {
     if (meta !== undefined) {
       message += ` - ${JSON.stringify(meta)}`;
@@ -21,10 +28,15 @@ function graphileWorkerLogFactory(): LogFunction {
         logger.log(message);
         break;
       case "debug":
-        logger.debug(message, meta);
+        logger.debug(message);
         break;
     }
   };
+}
+
+function graphileWorkerLogFactory(_scope?: object): LogFunction {
+  const logger = new Logger("GraphileWorker");
+  return createGraphileWorkerLogFunction(logger);
 }
 
 export const RunnerLogger = new GraphileLogger(graphileWorkerLogFactory);
